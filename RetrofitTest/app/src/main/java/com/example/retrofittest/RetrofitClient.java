48b1ba43c74c73dbab4ff50be2976cecbb8f61e2
khanhne1304 <chikhanhphanle2004@gmail.com>
2025-03-16 08:50:34 +0700

    retrofit

---
+++ RetrofitTest/app/src/main/java/com/example/retrofittest/RetrofitClient.java
@@ -0,0 +1,19 @@
+package com.example.retrofittest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    // đường dẫn API
+                    .baseUrl("http://app.iotstar.vn:8081/appfoods/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
